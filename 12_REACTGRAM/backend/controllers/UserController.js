7884d7d745d0bcefcc52847bef424f9036b676f4
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-10-20 23:28:38 -0300

    12_REACTGRAM - Validações, Login, Autenticação, Middlewares, Upload, Atualização de usuário e resgate de usuário por id

--- 12_REACTGRAM/backend/controllers/UserController.js
+++ 12_REACTGRAM/backend/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -13,9 +14,139 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  // check if user exists
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, utilize outro e-mail"] });
+    return;
+  }
+
+  // Generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // Create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash
+  });
+
+  // if user was created successfully, return the token
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+    return;
+  }
+
+  // Return user with token
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id)
+  });
+};
+
+// Sign user in
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // check if user exists
+  if (!user) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+
+  // check if password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida."] });
+    return;
+  }
+
+  // Return user with token
+  res.status(201).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id)
+  });
+};
+
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+
+  res.status(200).json(user);
+};
+
+// Update an user
+const update = async (req, res) => {
+  const { name, password, bio } = req.body;
+
+  let profileImage = null;
+
+  if (req.file) {
+    profileImage = req.file.filename;
+  }
+
+  const reqUser = req.user;
+  const objectId = new mongoose.Types.ObjectId(reqUser._id);
+  const user = await User.findById(objectId).select("-password");
+
+  if (name) {
+    user.name = name;
+  }
+
+  if (password) {
+    // Generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    user.password = passwordHash;
+  }
+
+  if (profileImage) {
+    user.profileImage = profileImage;
+  }
+
+  if (bio) {
+    user.bio = bio;
+  }
+
+  await user.save();
+
+  res.status(200).json(user);
+};
+
+// Get user by id
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const userId = new mongoose.Types.ObjectId(id);
+    const user = await User.findById(userId).select("-password");
+
+    // check if user exists
+    if (!user) {
+      res.status(404).json({ errors: ["Usuário não encontrado!"] });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ errors: ["Usuário não encontrado!"] });
+    return;
+  }
 };
 
 module.exports = {
-  register
+  register,
+  login,
+  getCurrentUser,
+  update,
+  getUserById
 };
