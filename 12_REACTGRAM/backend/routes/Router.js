8400c31219b20d8c4745a0cde3b3f78be533074f
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-10-21 01:54:22 -0300

    12_REACTGRAM - BackEnd Finalizado

--- 12_REACTGRAM/backend/routes/Router.js
+++ 12_REACTGRAM/backend/routes/Router.js
@@ -1,11 +1,12 @@
-const express = require("express")
+const express = require("express");
 const router = express();
 
 router.use("/api/users", require("./UserRoutes"));
+router.use("/api/photos", require("./PhotoRoutes"));
 
 // test router
 router.get("/", (req, res) => {
-    res.send("API WORKING");
-})
+  res.send("API WORKING");
+});
 
 module.exports = router;
